dc44919301204894ef21a1cd642feb7b83baa987
Felix Lin <linxz02@gmail.com>
2020-06-16 14:09:37 -0400

    lesson03: can echo msg

--- src/lesson03/src/kernel.c
+++ src/lesson03/src/kernel.c
@@ -8,10 +8,15 @@ void kernel_main(void)
 {
 	uart_init();
 	init_printf(0, putc);
+
+	printf("printf init ok\n");
+
 	irq_vector_init();
 	timer_init();
 	enable_interrupt_controller();
 	enable_irq();
+
+	printf("irq init ok\n");
 
 	while (1){
 		uart_send(uart_recv());
