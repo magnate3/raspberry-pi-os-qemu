3420b85f2ae21b971022b37ddc29c43cde7a8f72
xzl <linxz02@gmail.com>
2021-02-25 14:25:27 -0500

    up

--- src/lesson04a/src/kernel.c
+++ src/lesson04a/src/kernel.c
@@ -6,13 +6,40 @@
 #include "sched.h"
 #include "mini_uart.h"
 
+#ifdef USE_LFB
+#include "lfb.h"
+#endif
+
+#ifdef USE_QEMU
+#define CHAR_DELAY (5 * 5000000)
+#else
+#define CHAR_DELAY (1000000)
+#endif
+
 void process(char *array)
 {
+#ifdef USE_LFB // (optional) determine the init locations on the graphical console
+	int scr_x, scr_y; 
+	char c; 
+	if (array[0] == '1') {
+		scr_x = 0; scr_y = 320; 
+	} else {
+		scr_x = 0; scr_y = 480; 
+	}
+#endif 
+
 	while (1){
 		for (int i = 0; i < 5; i++){
 			uart_send(array[i]);
-			delay(5000000);
-		}
+#ifdef USE_LFB  // (optional) output to the graphical console
+			c = array[i+1]; array[i+1]='\0';
+			lfb_print_update(&scr_x, &scr_y, array+i);
+			array[i+1] = c; 
+			if (scr_x > 1024)
+				lfb_print_update(&scr_x, &scr_y, "\n");
+#endif
+			delay(CHAR_DELAY);
+		} 
 		schedule(); // yield
 	}
 }
@@ -22,12 +49,13 @@ void kernel_main(void)
 	uart_init();
 	init_printf(0, putc);
 
-	printf("kernel boots\n");
+	printf("kernel boots\r\n");	
 
-//	irq_vector_init();
-//	generic_timer_init();
-//	enable_interrupt_controller();
-//	enable_irq();
+#ifdef USE_LFB // (optional) init output to the graphical console
+	lfb_init(); 
+	lfb_showpicture();
+	lfb_print(0, 240, "kernel boots");
+#endif		
 
 	int res = copy_process((unsigned long)&process, (unsigned long)"12345");
 	if (res != 0) {
